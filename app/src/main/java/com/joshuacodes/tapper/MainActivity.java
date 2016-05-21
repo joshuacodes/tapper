6cd22d92404158a5f9b73829f8c503b0368163e7
Joshua Codes <joshuacodes@gmail.com>
2016-05-21 18:21:49 -0400

    Initial commit

---
+++ app/src/main/java/com/joshuacodes/tapper/MainActivity.java
@@ -0,0 +1,14 @@
+package com.joshuacodes.tapper;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
